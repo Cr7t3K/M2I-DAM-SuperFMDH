9841bc46be97e7d02a0415333f33332251551009
Guillaume <cvr.contact.pro@gmail.com>
2025-09-30 11:09:18 +0200

    Create base twig for homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,6 +8,107 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
+    const HOUSES = [
+        [
+            'id' => 1,
+            'price' => 489000,
+            'title' => 'Maison de ville rénovée - 4 pièces',
+            'short_description' => 'Séjour lumineux, cuisine équipée, 3 chambres, proche métro.',
+            'city' => 'Lyon',
+            'postal_code' => '69007',
+        ],
+        [
+            'id' => 2,
+            'price' => 415000,
+            'title' => 'Échoppe bordelaise avec patio',
+            'short_description' => 'Charme de l’ancien, pièce de vie ouverte, 2 chambres, au calme.',
+            'city' => 'Bordeaux',
+            'postal_code' => '33000',
+        ],
+        [
+            'id' => 3,
+            'price' => 359000,
+            'title' => 'Pavillon familial - 5 pièces',
+            'short_description' => 'Jardin clos, garage, 4 chambres, écoles accessibles à pied.',
+            'city' => 'Nantes',
+            'postal_code' => '44000',
+        ],
+        [
+            'id' => 4,
+            'price' => 795000,
+            'title' => 'Villa contemporaine avec piscine',
+            'short_description' => 'Grand séjour, suite parentale, terrain paysagé, quartier calme.',
+            'city' => 'Marseille',
+            'postal_code' => '13008',
+        ],
+        [
+            'id' => 5,
+            'price' => 649000,
+            'title' => 'Maison de plain-pied proche du lac',
+            'short_description' => 'Espace de vie traversant, 3 chambres, terrasse, stationnement.',
+            'city' => 'Annecy',
+            'postal_code' => '74000',
+        ],
+        [
+            'id' => 6,
+            'price' => 325000,
+            'title' => 'Maison avec jardin - 4 pièces',
+            'short_description' => 'Cuisine ouverte, 3 chambres, cave, quartier résidentiel.',
+            'city' => 'Grenoble',
+            'postal_code' => '38100',
+        ],
+    ];
+    const APARTMENTS = [
+        [
+            'id' => 1,
+            'price' => 289000,
+            'title' => 'Appartement moderne - 2 pièces',
+            'short_description' => 'Séjour lumineux, cuisine ouverte, 1 chambre, balcon.',
+            'city' => 'Paris',
+            'postal_code' => '75015',
+        ],
+        [
+            'id' => 2,
+            'price' => 198000,
+            'title' => 'Studio en centre-ville',
+            'short_description' => 'Entièrement équipé, idéal étudiant ou investissement locatif.',
+            'city' => 'Lille',
+            'postal_code' => '59000',
+        ],
+        [
+            'id' => 3,
+            'price' => 345000,
+            'title' => 'Loft au style industriel',
+            'short_description' => 'Grand espace ouvert, chambre en mezzanine, immeuble rénové.',
+            'city' => 'Lyon',
+            'postal_code' => '69002',
+        ],
+        [
+            'id' => 4,
+            'price' => 420000,
+            'title' => 'Appartement avec terrasse - 3 pièces',
+            'short_description' => 'Espace de vie spacieux, 2 chambres, terrasse avec vue sur la ville.',
+            'city' => 'Toulouse',
+            'postal_code' => '31000',
+        ],
+        [
+            'id' => 5,
+            'price' => 510000,
+            'title' => 'Penthouse avec vue panoramique',
+            'short_description' => 'Résidence de standing, grande terrasse, 2 chambres, garage.',
+            'city' => 'Nice',
+            'postal_code' => '06000',
+        ],
+        [
+            'id' => 6,
+            'price' => 265000,
+            'title' => 'Appartement proche université - 3 pièces',
+            'short_description' => 'Séjour avec balcon, 2 chambres, proche tram et commerces.',
+            'city' => 'Montpellier',
+            'postal_code' => '34000',
+        ],
+    ];
+
     #[Route(
         '/',
         name: 'app_home'
